d51a0bac26c86aa586bbd7b763088d964fb463b4
Conrad Lippert-Zajaczkowski <ceo@swapped.shop>
2023-07-12 22:37:02 -0500

    poetry

--- python_starter/__init__.py
+++ python_starter/__init__.py
@@ -5,11 +5,13 @@ from argparse import ArgumentParser, Namespace
 
 __version__ = "0.0.1"
 
+
 def parse_inputs() -> Namespace:
-   """Parses the user program input"""
-   parser = ArgumentParser(description='Add your arguments')
-   parser.add_argument('name', help='Add a name')
-   return parser.parse_args()
+    """Parses the user program input"""
+    parser = ArgumentParser(description="Add your arguments")
+    parser.add_argument("name", help="Add a name")
+    return parser.parse_args()
+
 
 def main() -> int:
     """Build many objects to process into our algolia database"""
@@ -17,5 +19,6 @@ def main() -> int:
     print(arguments_namespace.name)
     return 0
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     sys.exit(main())  # next section explains the use of sys.exit
